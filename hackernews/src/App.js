f9d414c5936c6a411b759644ef809e92babb7d90
Roy <luogao_lg@sina.com>
2018-03-09 18:16:40 +0800

    更新学习进展

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,75 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
+import Search from './Search'
+import Table from './Table'
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
+const DEFAULT_QUERY = 'redux'
 
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      list
+      result: null,
+      searchTerm: DEFAULT_QUERY
     }
+    this.setSearchTopStories = this.setSearchTopStories.bind(this)
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
+    this.onSearchchange = this.onSearchchange.bind(this)
+    this.onDismiss = this.onDismiss.bind(this)
   }
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result })
+  }
+  fetchSearchTopStories(searchTerm) {
+    fetch(url)
+      .then(res => res.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => e)
+  }
+  //delete item
   onDismiss(id) {
-    const updatedList = this.state.list.filter(el => el.objectID !== id)
+    const updatedList = this.state.result.hits.filter(el => el.objectID !== id)
     this.setState({ list: updatedList })
   }
+  onSearchchange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render() {
-    const user = {
-      firstName: 'Roy',
-      lastName: 'Luo'
-    }
-    function formatName(user) {
-      let output = user.firstName + ' ' + user.lastName
-      if (user) {
-        return (
-          <h1> HI, {output}!</h1>
-        )
-      } else {
-        return (
-          <h1> HI, Stranger. </h1>
-        )
-      }
-    }
+    const {
+      searchTerm,
+      result
+    } = this.state
+    if (!result) { return null; }
     return (
-      <div className="App">
-        {formatName(user)}
-        {this.state.list.map(item =>
-          <div key={item.objectID.toString()}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
-                Dismiss
-              </button>
-            </span>
-          </div>
-        )}
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchchange}
+          >
+            Search:
+          </Search>
+
+          <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} />
+        </div>
       </div>
     )
   }
---
+++ hackernews/src/Button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Button = ({ onClick, className = '', children }) =>
+    <button type="button" className={className} onClick={onClick}>{children}</button>
+
+export default Button;
